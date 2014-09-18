1608788b397fbdd96f8bf82532c75f0263de8c6a
cfdickmann <cfdickmann@googlemail.com>
2014-09-18 10:46:35 +0200

    Update meanvar.cpp

--- meanvar.cpp
+++ meanvar.cpp
@@ -1,9 +1,3 @@
-/*
- * File: meanvar.cpp
- * Author: dickmann
- *
- * Created on August 7, 2012, 11:30 AM
- */
 
 #include <cstdlib>
 #include <iostream>
@@ -12,103 +6,75 @@
 #include <cstring>
 #include <math.h>
 #include <vector>
-
 using namespace std;
-
 int main(int argc, char** argv) {
 	if (argc == 1) {
 		printf("Kein Dateiname angegeben\n");
 		exit(0);
 	}
-
 	vector<int> vec;
-
 	bool geaendert = false;
-	// int argnum=6666;
-	int laufmax = 1;
+	bool simple = false;
+
 	for (int i = 0; i < argc; ++i) {
 		string arg = argv[i];
 		bool geaendert = false;
 
-		if (!arg.compare("-c")) {
-			laufmax = 1000000;
+		if (!arg.compare("-s")) {
 			geaendert = true;
+			simple = true;
 		}
-
 		if (!geaendert) {
-			if(i>0)vec.push_back(i);
-			//    argnum=i;
+			if (i > 0)
+				vec.push_back(i);
+
 			geaendert = true;
 		}
-
 		if (i > 0 && !geaendert) {
 			printf("Unverständliche Parameter!\n");
 			exit(0);
 		}
 	}
 
-	// if(argnum==6666 || (laufmax=1000000 && argc==1)){
-	// printf("Keine Datei nicht oeffnen\n");
-	// exit(0);
-	// }
-
 	for (int kk = 0; kk < (int) vec.size(); ++kk) {
-
-		for (int lauf = 1; lauf <= laufmax; ++lauf) {
-			usleep(100000);
-			double x[100000];
-			string line;
-			ifstream myfile(argv[vec.at(kk)]);
-			int K = 0;
-			if (myfile.is_open()) {
-				while (!myfile.eof()) {
-					getline(myfile, line);
-					char buffer[line.length()];
-					strcpy(buffer, line.c_str());
-					if (strlen(buffer) > 0) {
-						x[K % 100000] = (double) (atof(buffer));
-						K++;
-					}
+		double x[100000];
+		string line;
+		ifstream myfile(argv[vec.at(kk)]);
+		int K = 0;
+		if (myfile.is_open()) {
+			while (!myfile.eof()) {
+				getline(myfile, line);
+				char buffer[line.length()];
+				strcpy(buffer, line.c_str());
+				if (strlen(buffer) > 0) {
+					x[K % 100000] = (double) (atof(buffer));
+					K++;
 				}
-				myfile.close();
-			} else {
-				printf("Kann Datei nicht oeffnen\n");
-				exit(0);
 			}
-
-			double mean = 0;
-			for (int i = 0; i < K; ++i)
-				mean += x[i] / (double) (K);
-			double quadratsumme = 0;
-			for (int k = 0; k < K; ++k)
-				quadratsumme += x[k] * x[k] / (double) (K);
-
+			myfile.close();
+		} else {
+			printf("Kann Datei nicht oeffnen\n");
+			exit(0);
+		}
+		double mean = 0;
+		for (int i = 0; i < K; ++i)
+			mean += x[i] / (double) (K);
+		double quadratsumme = 0;
+		for (int k = 0; k < K; ++k)
+			quadratsumme += x[k] * x[k] / (double) (K);
+		if (!simple) {
 			printf(" n: %d ", K);
 			printf(" mean: %.5lf, ", mean);
 			printf("stdev: %.8lf ",
 					sqrt(quadratsumme - mean * mean) * (double) (K)
 							/ (double) (K - 1));
 			printf(" %s ", argv[vec.at(kk)]);
-
-			if (lauf == laufmax)
-				printf("\n");
-			else {
-				if (lauf % 4 == 0)
-					printf("/");
-				if (lauf % 4 == 1)
-					printf("-");
-				if (lauf % 4 == 2)
-					printf("%c", 92);
-				if (lauf % 4 == 3)
-					printf("|");
-				printf("\r");
-			}
-
-			cout.flush();
+		} else {
+			printf("%.5lf,", mean);
 		}
+		printf("\n");
+		cout.flush();
+
 	}
 }
-
-
-
 
