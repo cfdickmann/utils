e238f1777426561f5e1cc42018ce695327d5180a
cfdickmann <cfdickmann@googlemail.com>
2014-09-18 11:45:48 +0200

    Update mse.cpp

--- mse.cpp
+++ mse.cpp
@@ -1,3 +1,4 @@
+
 #include <cstdlib>
 #include <iostream>
 #include <fstream>
@@ -15,23 +16,22 @@ int main(int argc, char** argv) {
 	}
 
 	vector<int> vec;
-
 	bool geaendert = false;
+	bool simple = false;
+
 	int argnum = 6666;
-	int laufmax = 1;
 	for (int i = 0; i < argc; ++i) {
 		string arg = argv[i];
 		bool geaendert = false;
 
-		if (!arg.compare("-c")) {
-			laufmax = 1000000;
+		if (!arg.compare("-s")) {
+			simple = true;
 			geaendert = true;
 		}
 
 		if (!geaendert) {
 			if (i > 0)
 				vec.push_back(i);
-//argnum=i;
 			geaendert = true;
 		}
 
@@ -41,60 +41,45 @@ int main(int argc, char** argv) {
 		}
 	}
 
-// if(argnum==6666 || (laufmax=1000000 && argc==1)){
-// printf("Keine Datei nicht oeffnen\n");
-// exit(0);
-// }
 	for (int kk = 0; kk < (int) vec.size(); ++kk) {
-		for (int lauf = 1; lauf <= laufmax; ++lauf) {
-			usleep(100000);
-			double x[100000];
-			string line;
-			ifstream myfile(argv[vec.at(kk)]);
-			int K = 0;
-			if (myfile.is_open()) {
-				while (!myfile.eof()) {
-					getline(myfile, line);
-					char buffer[line.length()];
-					strcpy(buffer, line.c_str());
-					if (strlen(buffer) > 0) {
-						x[K % 100000] = (double) (atof(buffer));
-						K++;
-					}
+		double x[100000];
+		string line;
+		ifstream myfile(argv[vec.at(kk)]);
+		int K = 0;
+		if (myfile.is_open()) {
+			while (!myfile.eof()) {
+				getline(myfile, line);
+				char buffer[line.length()];
+				strcpy(buffer, line.c_str());
+				if (strlen(buffer) > 0) {
+					x[K % 100000] = (double) (atof(buffer));
+					K++;
 				}
-				myfile.close();
-			} else {
-				printf("Kann Datei nicht oeffnen\n");
-				exit(0);
 			}
+			myfile.close();
+		} else {
+			printf("Kann Datei nicht oeffnen\n");
+			exit(0);
+		}
 
-			double mean = 0;
-			for (int i = 0; i < K; ++i)
-				mean += x[i] / (double) (K);
-			double quadratsumme = 0;
-			for (int k = 0; k < K; ++k)
-				quadratsumme += (16.006 - x[k]) * (16.006 - x[k])
-						/ (double) (K);
-
+		double mean = 0;
+		for (int i = 0; i < K; ++i)
+			mean += x[i] / (double) (K);
+		double quadratsumme = 0;
+		for (int k = 0; k < K; ++k)
+			quadratsumme += (16.006 - x[k]) * (16.006 - x[k]) / (double) (K);
+		if (!simple) {
 			printf(" n: %d ", K);
 			printf("mse: %.8lf, sqrt(mse) %.8lf ,  ", quadratsumme,
 					sqrt(quadratsumme));
 			printf(" %s ", argv[vec.at(kk)]);
+		} else
+			printf("%.8lf,", sqrt(quadratsumme));
 
-			if (lauf == laufmax)
-				printf("\n");
-			else {
-				if (lauf % 4 == 0)
-					printf("/");
-				if (lauf % 4 == 1)
-					printf("-");
-				if (lauf % 4 == 2)
-					printf("%c", 92);
-				if (lauf % 4 == 3)
-					printf("|");
-				printf("\r");
-			}
-			cout.flush();
-		}
+		printf("\n");
+
+		cout.flush();
+
 	}
 }
+
